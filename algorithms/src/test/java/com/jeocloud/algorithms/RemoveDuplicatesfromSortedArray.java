f1d890ba0db5fd4359a4576d67f97cf2ace0c061
steve <529543738@qq.com>
2016-08-11 15:11:24 +0800

    1.完成第二道题目

--- algorithms/src/test/java/com/jeocloud/algorithms/RemoveDuplicatesfromSortedArray.java
+++ algorithms/src/test/java/com/jeocloud/algorithms/RemoveDuplicatesfromSortedArray.java
@@ -10,7 +10,6 @@ public class RemoveDuplicatesfromSortedArray {
 		int length = 0;
 		int i = 0;  //当前遍历的位置
 		boolean issame = false;  //上个位置比较的结果
-		int last = nums[0];
 		//一次遍历一样的
 		for(; i < nums.length - 1; i++) {
 			if(!issame) {
@@ -30,15 +29,50 @@ public class RemoveDuplicatesfromSortedArray {
 		}
 		return length;
 	}
+	
+	public int removeDuplicates2(int[] nums) {
+		if(nums.length == 0) return 0;
+		int length = 0;
+		int i = 0;  //当前遍历的位置
+		int samenum = 0;  //上次比较结果，可能是0，说明上次比较不一样，可能是1，说明上次一样，也可能是2
+		for(; i < nums.length - 1; i++) {
+			if(samenum >= 0 && samenum <= 1) {
+				length++;
+				nums[length - 1] = nums[i];
+			}
+			if(nums[i] == nums[i+1]) {
+				samenum++;
+			}else {
+				samenum = 0;
+			}
+		}
+		if(samenum >= 0 && samenum <= 1) {
+			length++;
+			nums[length - 1] = nums[i];
+		}
+		return length;
+	}
+	
 	@Test
 	public void test() {
-		int[] nums = new int[5];
+		int[] nums = new int[6];
 		nums[0] = 1;
-		nums[1] = 1;
+		nums[1] = 3;
 		nums[2] = 3;
 		nums[3] = 3;
 		nums[4] = 4;
+		nums[5] = 4;
 		System.out.println(removeDuplicates(nums));
-		System.out.println(removeDuplicates(nums));
+	}
+	@Test
+	public void test2() {
+		int[] nums = new int[6];
+		nums[0] = 1;
+		nums[1] = 3;
+		nums[2] = 3;
+		nums[3] = 3;
+		nums[4] = 4;
+		nums[5] = 4;
+		System.out.println(removeDuplicates2(nums));
 	}
 }
